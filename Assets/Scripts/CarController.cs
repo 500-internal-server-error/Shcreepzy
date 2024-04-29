3ebd8b6513c6f6e781b05a845b3410c0c996ec81
500-internal-server-error <76838083+500-internal-server-error@users.noreply.github.com>
2024-04-29 19:27:53 +0700

    Removed dead code

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -5,18 +5,11 @@ public class CarController : MonoBehaviour
 {
     [Header("Model")]
 
-    [SerializeField] private GameObject model;
-
-    [SerializeField] private GameObject bodyModel;
-    [SerializeField] private GameObject wheelBLModel;
-    [SerializeField] private GameObject wheelBRModel;
     [SerializeField] private GameObject wheelFLModel;
     [SerializeField] private GameObject wheelFRModel;
 
     [Header("Physics")]
 
-    [SerializeField] private Rigidbody rb;
-    [SerializeField] private BoxCollider bodyCollider;
     [SerializeField] private WheelCollider wheelBLCollider;
     [SerializeField] private WheelCollider wheelBRCollider;
     [SerializeField] private WheelCollider wheelFLCollider;
@@ -44,17 +37,6 @@ public class CarController : MonoBehaviour
         Vector2 moveDirection = moveAction.ReadValue<Vector2>();
         float thrust = moveDirection.y;
         float steer = moveDirection.x;
-
-        // wheelBL.GetComponent<WheelCollider>().steerAngle = maxWheelAngle * steer;
-
-        rb.AddForce(thrust * power * transform.forward);
-
-        // rb.AddForceAtPosition(thrust * power /* / 4  */ * wheelFLModel.transform.forward, wheelFLModel.transform.position);
-        // rb.AddForceAtPosition(thrust * power /* / 4  */ * wheelFRModel.transform.forward, wheelFRModel.transform.position);
-        // rb.AddForceAtPosition(thrust * power /* / 4  */ * wheelBLModel.transform.forward, wheelBLModel.transform.position);
-        // rb.AddForceAtPosition(thrust * power /* / 4  */ * wheelBRModel.transform.forward, wheelBRModel.transform.position);
-
-        Debug.Log(rb.velocity);
 
         if (thrust > 0.05f || thrust < -0.05f)
         {
@@ -87,26 +69,15 @@ public class CarController : MonoBehaviour
 
         if (steer > 0.05f || steer < -0.05f)
         {
-            // bodyCollider.transform.rotation = Quaternion.Euler(0, steer * maxWheelAngle, 0);
-            // wheelBLCollider.steerAngle = steer * maxWheelAngle;
-            // wheelBRCollider.steerAngle = steer * maxWheelAngle;
             wheelFLCollider.steerAngle = steer * maxWheelAngle;
             wheelFRCollider.steerAngle = steer * maxWheelAngle;
         }
         else
         {
-            // bodyCollider.transform.rotation = Quaternion.Euler(0, 0, 0);
-            // wheelBLCollider.steerAngle = 0;
-            // wheelBRCollider.steerAngle = 0;
             wheelFLCollider.steerAngle = 0;
             wheelFRCollider.steerAngle = 0;
         }
 
-        // bodyModel.transform.rotation = bodyCollider.transform.rotation;
-        // wheelBLCollider.GetWorldPose(out var _, out Quaternion rotationBL);
-        // wheelBLModel.transform.rotation = rotationBL;
-        // wheelBRCollider.GetWorldPose(out var _, out Quaternion rotationBR);
-        // wheelBRModel.transform.rotation = rotationBR;
         wheelFLCollider.GetWorldPose(out var _, out Quaternion rotationFL);
         wheelFLModel.transform.rotation = rotationFL;
         wheelFRCollider.GetWorldPose(out var _, out Quaternion rotationFR);
