279b6cd1a364d09ed02f240d59e3a3a072bfcc6a
500-internal-server-error <76838083+500-internal-server-error@users.noreply.github.com>
2024-05-17 22:06:47 +0700

    Namespaceify everything

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,106 +1,109 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class CarController : MonoBehaviour
+namespace Shcreepzy
 {
-    [Header("Model")]
-
-    [SerializeField] private Rigidbody rb;
-
-    [SerializeField] private GameObject wheelFLModel;
-    [SerializeField] private GameObject wheelFRModel;
-
-    [SerializeField] private GameObject speedometerArrow;
-
-    [Header("Physics")]
-
-    [SerializeField] private WheelCollider wheelBLCollider;
-    [SerializeField] private WheelCollider wheelBRCollider;
-    [SerializeField] private WheelCollider wheelFLCollider;
-    [SerializeField] private WheelCollider wheelFRCollider;
-
-    [Header("Controls")]
-
-    [SerializeField] private PlayerInput playerInput;
-    private InputAction moveAction;
-
-    private Vector2 moveDirection;
-
-    [Header("Stats")]
-
-    [SerializeField, Range(0, 90)] private float maxWheelAngle;
-    [SerializeField, Min(0)] private float power;
-
-    private void Start()
+    public class CarController : MonoBehaviour
     {
-        moveAction = playerInput.actions["Move"];
-    }
+        [Header("Model")]
 
-    private void FixedUpdate()
-    {
-        float thrust = moveDirection.y;
-        float steer = moveDirection.x;
+        [SerializeField] private Rigidbody rb;
 
-        if (thrust > 0.05f || thrust < -0.05f)
+        [SerializeField] private GameObject wheelFLModel;
+        [SerializeField] private GameObject wheelFRModel;
+
+        [SerializeField] private GameObject speedometerArrow;
+
+        [Header("Physics")]
+
+        [SerializeField] private WheelCollider wheelBLCollider;
+        [SerializeField] private WheelCollider wheelBRCollider;
+        [SerializeField] private WheelCollider wheelFLCollider;
+        [SerializeField] private WheelCollider wheelFRCollider;
+
+        [Header("Controls")]
+
+        [SerializeField] private PlayerInput playerInput;
+        private InputAction moveAction;
+
+        private Vector2 moveDirection;
+
+        [Header("Stats")]
+
+        [SerializeField, Range(0, 90)] private float maxWheelAngle;
+        [SerializeField, Min(0)] private float power;
+
+        private void Start()
         {
-            wheelBLCollider.motorTorque = thrust * power;
-            wheelBLCollider.brakeTorque = 0;
-
-            wheelBRCollider.motorTorque = thrust * power;
-            wheelBRCollider.brakeTorque = 0;
-
-            wheelFLCollider.motorTorque = thrust * power;
-            wheelFLCollider.brakeTorque = 0;
-
-            wheelFRCollider.motorTorque = thrust * power;
-            wheelFRCollider.brakeTorque = 0;
-        }
-        else
-        {
-            wheelBLCollider.motorTorque = 0;
-            wheelBLCollider.brakeTorque = thrust * power;
-
-            wheelBRCollider.motorTorque = 0;
-            wheelBRCollider.brakeTorque = thrust * power;
-
-            wheelFLCollider.motorTorque = 0;
-            wheelFLCollider.brakeTorque = thrust * power;
-
-            wheelFRCollider.motorTorque = 0;
-            wheelFRCollider.brakeTorque = thrust * power;
+            moveAction = playerInput.actions["Move"];
         }
 
-        if (steer > 0.05f || steer < -0.05f)
+        private void FixedUpdate()
         {
-            wheelFLCollider.steerAngle = steer * maxWheelAngle;
-            wheelFRCollider.steerAngle = steer * maxWheelAngle;
-        }
-        else
-        {
-            wheelFLCollider.steerAngle = 0;
-            wheelFRCollider.steerAngle = 0;
+            float thrust = moveDirection.y;
+            float steer = moveDirection.x;
+
+            if (thrust > 0.05f || thrust < -0.05f)
+            {
+                wheelBLCollider.motorTorque = thrust * power;
+                wheelBLCollider.brakeTorque = 0;
+
+                wheelBRCollider.motorTorque = thrust * power;
+                wheelBRCollider.brakeTorque = 0;
+
+                wheelFLCollider.motorTorque = thrust * power;
+                wheelFLCollider.brakeTorque = 0;
+
+                wheelFRCollider.motorTorque = thrust * power;
+                wheelFRCollider.brakeTorque = 0;
+            }
+            else
+            {
+                wheelBLCollider.motorTorque = 0;
+                wheelBLCollider.brakeTorque = thrust * power;
+
+                wheelBRCollider.motorTorque = 0;
+                wheelBRCollider.brakeTorque = thrust * power;
+
+                wheelFLCollider.motorTorque = 0;
+                wheelFLCollider.brakeTorque = thrust * power;
+
+                wheelFRCollider.motorTorque = 0;
+                wheelFRCollider.brakeTorque = thrust * power;
+            }
+
+            if (steer > 0.05f || steer < -0.05f)
+            {
+                wheelFLCollider.steerAngle = steer * maxWheelAngle;
+                wheelFRCollider.steerAngle = steer * maxWheelAngle;
+            }
+            else
+            {
+                wheelFLCollider.steerAngle = 0;
+                wheelFRCollider.steerAngle = 0;
+            }
+
+            wheelFLCollider.GetWorldPose(out var _, out Quaternion rotationFL);
+            wheelFLModel.transform.rotation = rotationFL;
+            wheelFRCollider.GetWorldPose(out var _, out Quaternion rotationFR);
+            wheelFRModel.transform.rotation = rotationFR;
+
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, 8.0f);
+            if (rb.velocity.magnitude <= 2e-5f)
+            {
+                rb.velocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+            }
         }
 
-        wheelFLCollider.GetWorldPose(out var _, out Quaternion rotationFL);
-        wheelFLModel.transform.rotation = rotationFL;
-        wheelFRCollider.GetWorldPose(out var _, out Quaternion rotationFR);
-        wheelFRModel.transform.rotation = rotationFR;
-
-        rb.velocity = Vector3.ClampMagnitude(rb.velocity, 8.0f);
-        if (rb.velocity.magnitude <= 2e-5f)
+        private void Update()
         {
-            rb.velocity = Vector3.zero;
-            rb.angularVelocity = Vector3.zero;
+            moveDirection = moveAction.ReadValue<Vector2>();
+            speedometerArrow.transform.rotation = Quaternion.Euler(
+                0,
+                0,
+                120.0f - ((rb.velocity.magnitude * 240) / 8.0f) // adapted from https://stackoverflow.com/a/929107
+            );
         }
-    }
-
-    private void Update()
-    {
-        moveDirection = moveAction.ReadValue<Vector2>();
-        speedometerArrow.transform.rotation = Quaternion.Euler(
-            0,
-            0,
-            120.0f - ((rb.velocity.magnitude * 240) / 8.0f) // adapted from https://stackoverflow.com/a/929107
-        );
     }
 }
--- Assets/Scripts/SunController.cs
+++ Assets/Scripts/SunController.cs
@@ -1,19 +1,22 @@
 using UnityEngine;
 
-public class SunController : MonoBehaviour
+namespace Shcreepzy
 {
-    [SerializeField] private Light source;
-
-    // 0.02 s/t = 50 t/s = 15000 t / 5 m
-    private const int MAX_TIME = 15000;
-    [SerializeField, Range(0, MAX_TIME)] private int time;
-
-    [SerializeField] private bool doDaylightCycle;
-
-    private void FixedUpdate()
+    public class SunController : MonoBehaviour
     {
-        if (doDaylightCycle) time++;
-        if (time > MAX_TIME) time = 0;
-        source.transform.rotation = Quaternion.Euler(50, (float)time / (float)MAX_TIME * 360, 0);
+        [SerializeField] private Light source;
+
+        // 0.02 s/t = 50 t/s = 15000 t / 5 m
+        private const int MAX_TIME = 15000;
+        [SerializeField, Range(0, MAX_TIME)] private int time;
+
+        [SerializeField] private bool doDaylightCycle;
+
+        private void FixedUpdate()
+        {
+            if (doDaylightCycle) time++;
+            if (time > MAX_TIME) time = 0;
+            source.transform.rotation = Quaternion.Euler(50, (float)time / (float)MAX_TIME * 360, 0);
+        }
     }
 }
