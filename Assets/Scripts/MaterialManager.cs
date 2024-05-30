651927bc072b2363c1c227a4dbac2cb9c967b796
500-internal-server-error <76838083+500-internal-server-error@users.noreply.github.com>
2024-05-30 15:58:46 +0700

    Enabled cycling through quiz material

---
+++ Assets/Scripts/MaterialManager.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Shcreepzy
+{
+    public class MaterialManager : MonoBehaviour
+    {
+        [SerializeField] private Image slide;
+        [SerializeField] private List<Sprite> material;
+        private int currentMaterialIndex;
+
+        [SerializeField] private Button quitButton;
+        [SerializeField] private Button backButton;
+        [SerializeField] private Button nextButton;
+
+        private void OnEnable()
+        {
+            quitButton.onClick.AddListener(QuitMaterialScene);
+            backButton.onClick.AddListener(DisplayPreviousSlide);
+            nextButton.onClick.AddListener(DisplayNextSlide);
+        }
+
+        private void OnDisable()
+        {
+            quitButton.onClick.RemoveListener(QuitMaterialScene);
+            backButton.onClick.RemoveListener(DisplayPreviousSlide);
+            nextButton.onClick.RemoveListener(DisplayNextSlide);
+        }
+
+        private void Start()
+        {
+            currentMaterialIndex = 0;
+            slide.sprite = material[currentMaterialIndex];
+        }
+
+        private void QuitMaterialScene()
+        {
+            Debug.Log("Quitting");
+        }
+
+        private void DisplayPreviousSlide()
+        {
+            if (currentMaterialIndex > 0) currentMaterialIndex--;
+            slide.sprite = material[currentMaterialIndex];
+        }
+
+        private void DisplayNextSlide()
+        {
+            if (currentMaterialIndex < material.Count - 1) currentMaterialIndex++;
+            slide.sprite = material[currentMaterialIndex];
+        }
+    }
+}
