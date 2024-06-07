f19c5f8a6b44c15492a8b820c7cbdd680cf705d9
500-internal-server-error <76838083+500-internal-server-error@users.noreply.github.com>
2024-06-07 20:41:31 +0700

    Implemented quiz timer

--- Assets/Scripts/QuizManager.cs
+++ Assets/Scripts/QuizManager.cs
@@ -1,13 +1,25 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace Shcreepzy
 {
     public class QuizManager : MonoBehaviour
     {
+        [Header("References")]
+
         [SerializeField] private Canvas canvas;
+        [SerializeField] private TextMeshProUGUI timer;
         [SerializeField] private QuizQuestionObject quizQuestionPrefab;
         private QuizQuestionObject currentQuizQuestionObject;
+
+        [Header("Quiz Timer Settings")]
+
+        [SerializeField, Min(0), Tooltip("Quiz time in seconds")] private int maxQuizTime;
+        private int currentQuizTime;
+        private float lastSecond;
+
+        [Header("Quiz Question Settings")]
 
         [SerializeField, Min(0)] private int minimumCorrectAnswers;
         private int correctAnswers;
@@ -24,14 +36,41 @@ namespace Shcreepzy
         {
             currentQuestionIndex = 0;
             correctAnswers = 0;
+            currentQuizTime = maxQuizTime;
+            lastSecond = 0;
 
             if (currentQuizQuestionObject != null)
             {
                 Debug.LogWarning("currentQuestion is not null for some reason, destroying object");
-                Object.Destroy(currentQuizQuestionObject);
+                Object.Destroy(currentQuizQuestionObject.gameObject);
             }
 
             SpawnNextQuestion();
+        }
+
+        private void Update()
+        {
+            lastSecond += Time.deltaTime;
+            while (lastSecond >= 1)
+            {
+                lastSecond--;
+                currentQuizTime--;
+            }
+
+            if (currentQuizTime <= 0)
+            {
+                currentQuizTime = 0;
+                if (currentQuizQuestionObject != null)
+                {
+                    Debug.Log($"Out of time, {correctAnswers}/{quizQuestions.Count}");
+                    Object.Destroy(currentQuizQuestionObject.gameObject);
+                    currentQuizQuestionObject = null;
+                }
+            }
+
+            int minutes = currentQuizTime / 60;
+            int seconds = currentQuizTime % 60;
+            timer.text = $"{(minutes < 10 ? $"0{minutes}" : minutes)}:{(seconds < 10 ? $"0{seconds}" : seconds)}";
         }
 
         private void SpawnNextQuestion()
